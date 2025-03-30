60b413bb6d5686811f8294abbe8efea65f448913
dadhichharshit22 <dadhich.harshit22122002@gmail.com>
2025-03-30 23:10:49 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const app = express();
+const {Server} = require("socket.io");
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Socket.io connection
+io.on("connection",(socket)=>{
+    console.log("A new user has connected",socket.id);
+    socket.on("user-message",(message)=>{
+        console.log("A new User Message",message);
+        io.emit("message",message);
+    });
+}) 
+
+app.use(express.static(path.resolve("./public")));
+app.get("/",(req,res)=>{
+    return res.sendFile("./public/index.html");
+});
+
+server.listen(9000,()=>{console.log("Server is running at port number 9000")});
